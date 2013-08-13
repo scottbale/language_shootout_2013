a0ce27951b93307302d2c5628547b631cda57e59
Scott Bale <scott@balehaus.org>
2013-08-13 03:54:15 +0000

    haskell dna working

--- scottbale/nucleotide_and_prejudice/haskell/dna.hs
+++ scottbale/nucleotide_and_prejudice/haskell/dna.hs
@@ -1,12 +1,19 @@
 import qualified Data.Map as Map
+import Data.List
 
+countNucleotides :: String -> Map.Map Char Int
 countNucleotides word = foldr f counts word
                  where counts = Map.fromList [('A', 0), ('C', 0), ('G', 0), ('T', 0)]
                        f symbol cts = Map.insertWith (+) symbol 1 cts
 
-
-demo = countNucleotides "ACGCATGAAT"
+formattedCount :: Map.Map Char Int -> String
+formattedCount counts = (intersperse ' ') . concat . map show $ (map gitCount ['A', 'C', 'G', 'T'])
+               where gitCount ch = case Map.lookup ch counts of
+                                   Nothing -> 0
+                                   Just n -> n
+demo :: String
+demo = formattedCount $ countNucleotides "ACGCATGAAT"
 
 main :: IO ()
-main = print $ "yo " ++ show demo
+main = print $ "yo " ++ demo
 
